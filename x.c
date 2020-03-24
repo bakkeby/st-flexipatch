be0f0d56e58510cdb1a9a3acb1f82c8c37a7b48e
bakkeby <bakkeby@gmail.com>
2020-03-24 14:17:17 +0100

    apply hints before initial mapping (ICCCM) (843cfe)

--- x.c
+++ x.c
@@ -1357,8 +1357,8 @@ xinit(int cols, int rows)
 
 	win.mode = MODE_NUMLOCK;
 	resettitle();
-	XMapWindow(xw.dpy, xw.win);
 	xhints();
+	XMapWindow(xw.dpy, xw.win);
 	XSync(xw.dpy, False);
 
 	clock_gettime(CLOCK_MONOTONIC, &xsel.tclick1);
