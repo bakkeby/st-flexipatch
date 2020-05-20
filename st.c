7a1259e1121bb6474014cdfd5cc0295af22db6a7
bakkeby <bakkeby@gmail.com>
2020-05-20 14:26:04 +0200

    code-style: add fallthrough comment
    Patch by Steve Ward, thanks.

--- st.c
+++ st.c
@@ -2315,6 +2315,7 @@ tcontrolcode(uchar ascii)
 		return;
 	case '\032': /* SUB */
 		tsetchar('?', &term.c.attr, term.c.x, term.c.y);
+		/* FALLTHROUGH */
 	case '\030': /* CAN */
 		csireset();
 		break;
