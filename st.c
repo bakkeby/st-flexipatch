0c4e48dce364354cf5594559fe41509e46d781d6
bakkeby <bakkeby@gmail.com>
2022-01-04 14:29:27 +0100

    Upstream change: follow-up fix for OSC color sequences, return
    Otherwise the message "erresc: unknown str" is printed.
    
    https://git.suckless.org/st/commit/273db5ceaf392e68c2faf8f7dec14ea2e25e980d.html

--- st.c
+++ st.c
@@ -2955,7 +2955,7 @@ strhandle(void)
 				fprintf(stderr, "erresc: invalid foreground color: %s\n", p);
 			else
 				redraw();
-			break;
+			return;
 		case 11:
 			if (narg < 2)
 				break;
@@ -2968,7 +2968,7 @@ strhandle(void)
 				fprintf(stderr, "erresc: invalid background color: %s\n", p);
 			else
 				redraw();
-			break;
+			return;
 		case 12:
 			if (narg < 2)
 				break;
@@ -2981,7 +2981,7 @@ strhandle(void)
 				fprintf(stderr, "erresc: invalid cursor color: %s\n", p);
 			else
 				redraw();
-			break;
+			return;
 		case 4: /* color set */
 			if ((par == 4 && narg < 3) || narg < 2)
 				break;
