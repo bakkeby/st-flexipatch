90aae261d4ca9b0ba34906bdefed3332c47425fd
Fernando Vega <fernando.vega@smartwaterplanet.com>
2022-10-19 13:32:19 +0200

    Define FULLSCREEN_PATCH

--- patches.def.h
+++ patches.def.h
@@ -178,7 +178,15 @@
  * So they will be used first for glyphs that are absent in the default font.
  * https://st.suckless.org/patches/font2/
  */
-#define FONT2_PATCH 0
+#define FONT2_PATCH      0
+
+/*
+ * Add the ability to toggle st into fullscreen mode.
+ * Two key bindings are defined: F11 which is typical with other applications and ALT+ENTER
+ * which matches default xterm behavior.
+ * https://st.suckless.org/patches/fullscreen/
+ */
+#define FULLSCREEN_PATCH 0
 
 /* Hide the X cursor whenever a key is pressed and show it back when the mouse is moved in
  * the terminal window.
